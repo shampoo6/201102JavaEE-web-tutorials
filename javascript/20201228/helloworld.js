1e2f1215b06ccaed1c82944a25caa14c13482729
shampoo6 <shampoo6@163.com>
2020-12-28 09:30:42 +0800

    update

---
+++ javascript/20201228/helloworld.js
@@ -0,0 +1,14 @@
+// class Main {
+//     public static void main(String[] args){
+//         System.out.println('Hello World');
+//     }
+// }
+
+// js 代码从上至下，从左至右执行
+// console：控制台对象
+console.log('Hello World');
+console.info('info');
+console.warn('warn');
+console.error('error');
+// dir 专门用于显示文档对象的对象结构
+// console.dir(document.body)
